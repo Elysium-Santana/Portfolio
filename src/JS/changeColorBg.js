132ec5dd86c722324d296bba5e3a008ed327a19c
Elysyum <camaleplata@outlook.com>
2023-02-21 00:01:55 -0300

    ajustes na responsividade na seção contato

--- src/JS/changeColorBg.js
+++ src/JS/changeColorBg.js
@@ -6,16 +6,30 @@ export function changeColorBg() {
   });
   const listContact = document.querySelectorAll('.contactSectionContent li');
   const contactSection = document.querySelector('.contactSection');
-  console.log(listContact);
+  const mobileConfig = window.matchMedia('(max-width: 650px)');
 
-  listContact.forEach((item) => {
-    item.addEventListener('mouseenter', ({ target }) => {
-      const color = target.getAttribute('data-color');
-      contactSection.style.backgroundColor = color;
-    });
-    item.addEventListener('mouseleave', ({ target }) => {
-      const color = target.getAttribute('data-color');
-      contactSection.style.backgroundColor = '#1e201d';
-    });
-  });
+  function changeBg(mobileConfig) {
+    if (!mobileConfig.matches) {
+      listContact.forEach((item) => {
+        item.addEventListener('mouseenter', handleMouseEnter);
+        item.addEventListener('mouseleave', handleMouseLeave);
+      });
+      //aqui eu quero que execute somente quando for maior que 650px
+    } else {
+      listContact.forEach((item) => {
+        item.removeEventListener('mouseenter', handleMouseEnter);
+        item.removeEventListener('mouseleave', handleMouseLeave);
+      });
+    }
+  }
+
+  function handleMouseLeave() {
+    contactSection.style.backgroundColor = '#1e201d';
+  }
+  function handleMouseEnter(event) {
+    const color = event.target.getAttribute('data-color');
+    contactSection.style.backgroundColor = color;
+  }
+  mobileConfig.addEventListener('change', changeBg);
+  //o problema é que funciona uma vez so. quendo eu aumento a tela (mobileConfig.matches) continua como true
 }
